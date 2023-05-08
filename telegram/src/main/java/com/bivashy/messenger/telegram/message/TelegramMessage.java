c01e2769a76353b721d8c11e6a03b3db117b9590
bivashy <botyrbojey@gmail.com>
2023-05-08 21:17:19 +0600

    Prevent code duplicate

--- telegram/src/main/java/com/bivashy/messenger/telegram/message/TelegramMessage.java
+++ telegram/src/main/java/com/bivashy/messenger/telegram/message/TelegramMessage.java
@@ -9,6 +9,7 @@ import com.bivashy.messenger.common.ApiProvider;
 import com.bivashy.messenger.common.file.MessengerFile;
 import com.bivashy.messenger.common.identificator.Identificator;
 import com.bivashy.messenger.common.message.DefaultMessage;
+import com.bivashy.messenger.telegram.message.keyboard.TelegramKeyboard;
 import com.bivashy.messenger.telegram.providers.TelegramApiProvider;
 import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.request.BaseRequest;
@@ -18,13 +19,20 @@ import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.request.SendPhoto;
 import com.pengrad.telegrambot.request.SendVideo;
 import com.pengrad.telegrambot.response.SendResponse;
-import com.bivashy.messenger.telegram.message.keyboard.TelegramKeyboard;
 
 public class TelegramMessage extends DefaultMessage {
     private static TelegramApiProvider defaultApiProvider;
 
     public TelegramMessage(String text) {
         super(text);
+    }
+
+    public static TelegramApiProvider getDefaultApiProvider() {
+        return defaultApiProvider;
+    }
+
+    public static void setDefaultApiProvider(TelegramApiProvider defaultApiProvider) {
+        TelegramMessage.defaultApiProvider = defaultApiProvider;
     }
 
     @Override
@@ -44,15 +52,8 @@ public class TelegramMessage extends DefaultMessage {
 
     @Override
     public void send(Identificator identificator, ApiProvider apiProvider) {
-        SendMessage sendMessage = new SendMessage(identificator.asObject(), text);
-        if (keyboard != null && keyboard.safeAs(TelegramKeyboard.class).isPresent())
-            sendMessage.replyMarkup(keyboard.as(TelegramKeyboard.class).create());
-        if (replyIdentificator != null && replyIdentificator.isNumber())
-            sendMessage.replyToMessageId((int) replyIdentificator.asNumber());
-        apiProvider.as(TelegramApiProvider.class).getBot().execute(sendMessage);
-        if (files != null && files.length != 0)
-            toEntities(identificator, files)
-                    .forEach(request -> apiProvider.as(TelegramApiProvider.class).getBot().execute(request));
+        send(identificator, apiProvider, (ignored) -> {
+        });
     }
 
     public void send(Identificator identificator, ApiProvider apiProvider, Consumer<SendResponse> responseConsumer) {
@@ -70,6 +71,7 @@ public class TelegramMessage extends DefaultMessage {
 
                     @Override
                     public void onFailure(SendMessage request, IOException e) {
+                        e.printStackTrace();
                     }
                 });
         if (files != null && files.length != 0)
@@ -85,14 +87,6 @@ public class TelegramMessage extends DefaultMessage {
                 requests.add(request);
         }
         return requests;
-    }
-
-    public static TelegramApiProvider getDefaultApiProvider() {
-        return defaultApiProvider;
-    }
-
-    public static void setDefaultApiProvider(TelegramApiProvider defaultApiProvider) {
-        TelegramMessage.defaultApiProvider = defaultApiProvider;
     }
 
     private BaseRequest<?, ?> toRequest(Identificator identificator, MessengerFile file) {
