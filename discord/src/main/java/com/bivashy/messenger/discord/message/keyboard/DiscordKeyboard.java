c0cb4e3b51ef45c09965670174cc5d022fa4c7e4
bivashy <botyrbojey@gmail.com>
2023-06-17 23:18:03 +0600

    Make DiscordKeyboard precompute-able

--- discord/src/main/java/com/bivashy/messenger/discord/message/keyboard/DiscordKeyboard.java
+++ discord/src/main/java/com/bivashy/messenger/discord/message/keyboard/DiscordKeyboard.java
@@ -9,7 +9,18 @@ import com.bivashy.messenger.discord.message.keyboard.button.DiscordButton;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 
 public class DiscordKeyboard extends DefaultKeyboard {
+    private Collection<ActionRow> actionRows;
+
+    public DiscordKeyboard() {
+    }
+
+    public DiscordKeyboard(Collection<ActionRow> actionRows) {
+        this.actionRows = actionRows;
+    }
+
     public Collection<ActionRow> create() {
+        if (actionRows != null)
+            return actionRows;
         return buttons.stream()
                 .map(buttonList -> buttonList.stream().map(button -> button.as(DiscordButton.class).create()).collect(Collectors.toList()))
                 .map(ActionRow::of)
